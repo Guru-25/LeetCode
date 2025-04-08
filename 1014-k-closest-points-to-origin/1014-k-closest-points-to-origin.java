ea5ab1ac23f0dad8586573afb331001ebebab38d
Guru <gururaja.spr@gmail.com>
2025-04-09 00:32:49 +0530

    1014-K-CLOSEST-POINTS-TO-ORIGIN Stats: Time: 21 ms (91.53%), Space: 47.8 MB (97.39%) - LeetHub

--- 1014-k-closest-points-to-origin/1014-k-closest-points-to-origin.java
+++ 1014-k-closest-points-to-origin/1014-k-closest-points-to-origin.java
@@ -1,32 +1,33 @@
 class Solution {
     public int[][] kClosest(int[][] points, int k) {
-        // PriorityQueue<int[]> pq=new PriorityQueue<>((a,b)->{
-        //     return (b[0]*b[0] + b[1]*b[1]) - (a[0]*a[0] + a[1]*a[1]);
-        // });
-        // for(int[] point:points){
-        //     pq.add(point);
-        //     if(pq.size()>k){
-        //         pq.poll();
-        //     }
-        // }
-        // int[][] res=new int[k][2];
-        // for(int i=0;i<k;i++){
-        //     res[i]=pq.poll();
-        // }
-        // return res;
+        PriorityQueue<int[]> pq=new PriorityQueue<>((a, b) -> {
+            return (a[0]*a[0] + a[1]*a[1]) - (b[0]*b[0] + b[1]*b[1]);
+        });
 
-        // Min-heap based on distance from origin
-    PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> {
-        return (a[0]*a[0] + a[1]*a[1]) - (b[0]*b[0] + b[1]*b[1]);
-    });
+        for(int[] point:points){
+            pq.add(point);
+        }
 
-    for (int[] point : points)
-        pq.add(point); // O(log n)
+        int[][] res=new int[k][2];
+        for (int i=0;i<k;i++){
+            res[i]=pq.poll();
+        }
 
-    int[][] res = new int[k][2];
-    for (int i = 0; i < k; i++)
-        res[i] = pq.poll(); // Closest point comes first
+        return res;
 
-    return res;
+    // PriorityQueue<int[]> pq=new PriorityQueue<>((a,b)->{
+    //     return (b[0]*b[0] + b[1]*b[1]) - (a[0]*a[0] + a[1]*a[1]);
+    // });
+    // for(int[] point:points){
+    //     pq.add(point);
+    //     if(pq.size()>k){
+    //         pq.poll();
+    //     }
+    // }
+    // int[][] res=new int[k][2];
+    // for(int i=0;i<k;i++){
+    //     res[i]=pq.poll();
+    // }
+    // return res;
     }
 }
